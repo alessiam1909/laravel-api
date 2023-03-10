bcb12bb84682fcd1f3dc65966a9ed01149faa2e1
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-10 09:41:21 +0100

    aggiunta metodo index al postcontroller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+ use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+            $posts = Project::all();
+            return response()->json([
+                'success' => true,
+                'results' => $posts,
+            ]);
+        }
 }

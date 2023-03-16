4115c1291dd2de8bbb8ceff51e536b93dc9d5344
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-16 17:12:19 +0100

    aggiunta funzione nel controller per inviare la mail

--- app/Http/Controllers/Api/GuestLeadController.php
+++ app/Http/Controllers/Api/GuestLeadController.php
@@ -4,7 +4,38 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\GuestLead;
+use App\Mail\GuestContact;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
 class GuestLeadController extends Controller
 {
-    
+    public function store(Request $request){
+        $form_data = $request->all();
+        $validator = Validator::make($form_data, [
+            'name' => 'required',
+            'surname' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'message' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        };
+
+        $newContact = new GuestLead();
+        $newContact->fill($newContact);
+        $newContact->save();
+
+        Mail::to('info@boolpress.com')->send(new GuestContact($newContact));
+
+        return response()->json([
+            'success'=> true
+        ]);
+    }
 }
